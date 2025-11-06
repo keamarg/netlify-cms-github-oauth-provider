8e63af625d6e70783a6afe1876863a0fc267f3d4
Martin Gundtoft <marg@kea.dk>
2025-11-06 15:12:42 +0100

    sss

--- index.js
+++ index.js
@@ -1,34 +1,30 @@
-const simpleOauthModule = require('simple-oauth2')
-const authMiddleWareInit = require('./auth.js')
-const callbackMiddleWareInit = require('./callback')
-const oauthProvider = process.env.OAUTH_PROVIDER || 'github'
-const loginAuthTarget = process.env.AUTH_TARGET || '_self'
+const simpleOauthModule = require("simple-oauth2");
+const authMiddleWareInit = require("./auth.js");
+const callbackMiddleWareInit = require("./callback");
+const oauthProvider = process.env.OAUTH_PROVIDER || "github";
 
-const config = {
+const oauth2 = simpleOauthModule.create({
   client: {
     id: process.env.OAUTH_CLIENT_ID,
-    secret: process.env.OAUTH_CLIENT_SECRET
+    secret: process.env.OAUTH_CLIENT_SECRET,
   },
   auth: {
-    // Supply GIT_HOSTNAME for enterprise github installs.
-    tokenHost: process.env.GIT_HOSTNAME || 'https://github.com',
-    tokenPath: process.env.OAUTH_TOKEN_PATH || '/login/oauth/access_token',
-    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || '/login/oauth/authorize'
-  }
-}
+    tokenHost: process.env.GIT_HOSTNAME || "https://github.com",
+    tokenPath: process.env.OAUTH_TOKEN_PATH || "/login/oauth/access_token",
+    authorizePath: process.env.OAUTH_AUTHORIZE_PATH || "/login/oauth/authorize",
+  },
+});
 
-const oauth2 = new simpleOauthModule.AuthorizationCode(config)
-
-function indexMiddleWare (req, res) {
+function indexMiddleWare(req, res) {
   res.send(`Hello<br>
-    <a href="/auth" target="${loginAuthTarget}">
+    <a href="/auth" target="${process.env.AUTH_TARGET || "_self"}">
       Log in with ${oauthProvider.toUpperCase()}
-    </a>`)
+    </a>`);
 }
 
 module.exports = {
   auth: authMiddleWareInit(oauth2),
-  callback: callbackMiddleWareInit(oauth2, oauthProvider),
-  success: (req, res) => { res.send('') },
-  index: indexMiddleWare
-}
+  callback: callbackMiddleWareInit(oauth2, oauthProvider), // <-- pass providerName here
+  success: require("./success"), // your postMessage page
+  index: indexMiddleWare,
+};
