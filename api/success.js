f055f71f1c7a8e1c52f4bbb3b5df000c2ce43a55
Martin Gundtoft <marg@kea.dk>
2025-11-06 15:31:23 +0100

    bbb

--- api/success.js
+++ api/success.js
@@ -1,27 +1,2 @@
-// success.js
-module.exports = (req, res) => {
-  const html = `<!doctype html><meta charset="utf-8">
-    <title>Authorized</title>
-    <p>You are being redirected to the authorized application…</p>
-    <script>
-      (function () {
-        var hash = (location.hash || '').slice(1); // access_token=...&token_type=...
-        var msg = 'authorization:github:' + hash;
-        try {
-          if (window.opener && !window.opener.closed) {
-            window.opener.postMessage(msg, '*');
-            window.close();
-          } else {
-            document.body.innerHTML =
-              '<pre>' + msg + '</pre><p>Copy this to the app if the window did not close.</p>';
-          }
-        } catch (e) {
-          console.error(e);
-          document.body.innerHTML =
-            '<p>Authentication succeeded, but automatic redirect failed. You can close this window.</p>';
-        }
-      })();
-    </script>`;
-  res.setHeader("content-type", "text/html; charset=utf-8");
-  res.end(html);
-};
+const provider = require("../index.js");
+module.exports = (req, res) => provider.success(req, res);
